348ff03d9104fc3f52641042d9f6cfe4bf31bffc
kenpos <kenpos13@gmail.com>
2019-06-17 22:35:46 +0900

    正規表現を使って電話番号を抽出する

--- phoneNumberSearch.py
+++ phoneNumberSearch.py
@@ -8,3 +8,12 @@ phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phone_num_regex.search('私の電話番号は415-555-4242ですわよ．')
 
 print('電話番号:'+ mo.group())
+
+#正規表現でグループを
+phone_num_group_regex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+
+mo = phone_num_group_regex.search('私の電話番号は415-555-4242ですわよ．')
+
+print('電話番号:'+ mo.group(0))
+print('電話番号:'+ mo.group(1))
+print('電話番号:'+ mo.group(2))
