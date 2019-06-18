e7ac2c3ad8bc19d03d8d556c503573567d9d5d20
kenpos <kenpos13@gmail.com>
2019-06-18 22:55:28 +0900

    文字列置換

--- subMethod.py
+++ subMethod.py
@@ -5,3 +5,9 @@ import re
 name_regex = re.compile(r'Agent \w+')
 mo = name_regex.sub('CENSORED', 'Agent Alice gave the secret douments to Agent Bob.')
 print(mo)
+
+#\1はグループ１にマッチした文字列
+name_regex = re.compile(r'Agent (\w)\w*')
+mo1 = name_regex.sub('\1****', 'Agent Alice told Agent carol that Agent Eve knew Agent Nob was a double agent gave the secret douments to Agent Bob.')
+print(mo1)
+
